165e6d344a9ea1dd07f169c66f58b12882402855
Sergio CONGIA <sergio.congia@sap.com>
2022-05-20 15:53:14 +0400

    tests

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for eskomloadshedding integration."""
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,27 @@
 """Test component setup."""
-from homeassistant.setup import async_setup_component
-
+from custom_components.eskomloadshedding import (  # async_unload_entry,
+    EskomLoadsheddingDataCoordinator,
+    async_reload_entry,
+    async_setup_entry,
+)
 from custom_components.eskomloadshedding.const import DOMAIN
 
 
-async def test_async_setup(hass):
-    """Test the component gets setup."""
-    assert await async_setup_component(hass, DOMAIN, {}) is True
+async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
+    """Test entry setup and unload."""
+    # Create a mock entry so we don't have to go through config flow
+    # config_entry = MockConfigEntry(
+    #     domain=DOMAIN, data={}, entry_id=""
+    # )
+
+    # Set up the entry and assert that the values set during setup are where we expect
+    # them to be. Because we have patched the ocppDataUpdateCoordinator.async_get_data
+    # call, no code from custom_components/ocpp/api.py actually runs.
+    assert await async_setup_entry(hass)
+    assert DOMAIN in hass.data
+    assert type(hass.data[DOMAIN]) == EskomLoadsheddingDataCoordinator
+
+    # Reload the entry and assert that the data from above is still there
+    assert await async_reload_entry(hass) is None
+    assert DOMAIN in hass.data
+    assert type(hass.data[DOMAIN]) == EskomLoadsheddingDataCoordinator
