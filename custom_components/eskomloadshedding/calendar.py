a84f7e0a6a6a8d26a832ce3ef81574fd87739bb1
Sergio CONGIA <sergio.congia@sap.com>
2022-05-22 20:06:23 +0400

    Change to CalendarEvent

--- custom_components/eskomloadshedding/calendar.py
+++ custom_components/eskomloadshedding/calendar.py
@@ -4,22 +4,20 @@ from __future__ import annotations
 from datetime import datetime, timedelta, timezone
 from typing import Any
 
-from homeassistant.components.calendar import CalendarEventDevice
+from homeassistant.components.calendar import CalendarEntity, CalendarEvent
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ATTRIBUTION, CONF_ID
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-)
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import EskomLoadsheddingDataCoordinator
 from .const import (
+    ATTR_CALENDAR_EVENT_SUMMARY,
     ATTR_CALENDAR_ICON,
     ATTR_CALENDAR_ID,
     ATTR_CALENDAR_NAME,
     ATTR_SCHEDULE,
-    ATTR_CALENDAR_EVENT_SUMMARY,
     ATTRIBUTION,
     DOMAIN,
 )
@@ -32,13 +30,12 @@ async def async_setup_entry(
 ) -> None:
     """Set up Twente Milieu calendar based on a config entry."""
     eskom_loadshedding_coordinator = hass.data[DOMAIN]
-    async_add_entities(
-        [EskomLoadsheddingCalendar(eskom_loadshedding_coordinator)]
-    )
+    async_add_entities([EskomLoadsheddingCalendar(eskom_loadshedding_coordinator)])
 
 
-
-class EskomLoadsheddingCalendar( CoordinatorEntity[EskomLoadsheddingDataCoordinator], CalendarEventDevice ):
+class EskomLoadsheddingCalendar(
+    CoordinatorEntity[EskomLoadsheddingDataCoordinator], CalendarEntity
+):
     """Defines a Twente Milieu calendar."""
 
     _attr_name = ATTR_CALENDAR_NAME
@@ -53,55 +50,72 @@ class EskomLoadsheddingCalendar( CoordinatorEntity[EskomLoadsheddingDataCoordina
         super().__init__(coordinator)
         self._attr_unique_id = ATTR_CALENDAR_ID
         self._attrs = {ATTR_ATTRIBUTION: ATTRIBUTION}
-        self._event: dict[str, Any] | None = None
+        self._event: CalendarEvent | None = None
 
     @property
-    def event(self) -> dict[str, Any] | None:
+    def event(self) -> CalendarEvent | None:
         """Return the next upcoming event."""
         return self._event
 
     async def async_get_events(
         self, hass: HomeAssistant, start_date: datetime, end_date: datetime
-    ) -> list[dict[str, Any]]:
+    ) -> list[CalendarEvent]:
         """Return calendar events within a datetime range."""
-        events: list[dict[str, Any]] = []
+        events: list[CalendarEvent] = []
 
-        schedule = self.coordinator.data[ATTR_SCHEDULE]
-        utc_tz = timezone.utc
-        days = 7
+        if self.coordinator.data is not None:
+            schedule = self.coordinator.data[ATTR_SCHEDULE]
+            utc_tz = timezone.utc
+            days = 7
 
-        for s in schedule:
-            start = datetime.fromisoformat(s[0])
-            end = datetime.fromisoformat(s[1])
-            if start.date() > datetime.now(utc_tz).date() + timedelta(days=days):
-                continue
-            if end.date() < datetime.now(utc_tz).date():
-                continue
-            event = {
-                "all_day": False,
-                "start": {"dateTime": start.isoformat()},
-                "end": {"dateTime": end.isoformat()},
-                "summary": ATTR_CALENDAR_EVENT_SUMMARY
-            }
-            events.append(event)
+            for s in schedule:
+                start = datetime.fromisoformat(s[0])
+                end = datetime.fromisoformat(s[1])
+                if start.date() > datetime.now(utc_tz).date() + timedelta(days=days):
+                    continue
+                if end.date() < datetime.now(utc_tz).date():
+                    continue
+                events.append(
+                    CalendarEvent(
+                        summary=ATTR_CALENDAR_EVENT_SUMMARY,
+                        start=start,
+                        end=end,
+                    )
+                )
+                # event = {
+                #     "all_day": False,
+                #     "start": {"dateTime": start.isoformat()},
+                #     "end": {"dateTime": end.isoformat()},
+                #     "summary": ATTR_CALENDAR_EVENT_SUMMARY
+                # }
+                # events.append(event)
 
         return events
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        date_time_format = '%Y-%m-%d %H:%M'
+        date_time_format = "%Y-%m-%d %H:%M"
 
         self._event = None
         if self.coordinator.data is not None:
             if len(self.coordinator.data[ATTR_SCHEDULE]) > 0:
-                date_time_start = datetime.strptime(self.coordinator.data[ATTR_SCHEDULE][0][0],date_time_format)
-                date_time_end = datetime.strptime(self.coordinator.data[ATTR_SCHEDULE][0][1],date_time_format)
-                self._event = {
-                        "all_day": False,
-                        "start": {"dateTime": date_time_start.isoformat()},
-                        "end": {"dateTime": date_time_end.isoformat()},
-                    }
+                date_time_start = datetime.strptime(
+                    self.coordinator.data[ATTR_SCHEDULE][0][0], date_time_format
+                )
+                date_time_end = datetime.strptime(
+                    self.coordinator.data[ATTR_SCHEDULE][0][1], date_time_format
+                )
+                self._event = CalendarEvent(
+                    summary=ATTR_CALENDAR_EVENT_SUMMARY,
+                    start=date_time_start,
+                    end=date_time_end,
+                )
+                # self._event = {
+                #     "all_day": False,
+                #     "start": {"dateTime": date_time_start.isoformat()},
+                #     "end": {"dateTime": date_time_end.isoformat()},
+                # }
 
         super()._handle_coordinator_update()
 
